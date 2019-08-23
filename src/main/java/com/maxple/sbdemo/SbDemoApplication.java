ab62bfab973e507ecdbd5b1a2944144d2367931f
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2019-08-23 12:29:13 +0300

    Initial commit

---
+++ src/main/java/com/maxple/sbdemo/SbDemoApplication.java
@@ -0,0 +1,14 @@
+package com.maxple.sbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDemoApplication.class, args);
+    }
+
+}
