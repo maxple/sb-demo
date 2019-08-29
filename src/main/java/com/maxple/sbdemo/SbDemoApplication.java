61828bea4d7d607c14b4da7717fb70f67952cab9
Плетнев Максим Леонидович <Pletnev.M.Le@sberbank.ru>
2019-08-29 17:36:25 +0300

    add cat

--- src/main/java/com/maxple/sbdemo/SbDemoApplication.java
+++ src/main/java/com/maxple/sbdemo/SbDemoApplication.java
@@ -2,6 +2,12 @@ package com.maxple.sbdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 
 @SpringBootApplication
@@ -10,5 +16,42 @@ public class SbDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SbDemoApplication.class, args);
     }
+
+}
+
+@Entity
+class Cat {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+@RepositoryRestResource(path = "/cats")
+interface CatRepository extends JpaRepository<Cat, Long> {
 
 }
